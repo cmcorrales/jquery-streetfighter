19fe7a940901a2f2f95d3c35a34ba46911a4da09
Cassandra <cmcorrales3@gmail.com>
2016-06-26 11:21:23 -0500

    add control directions

--- js/app.js
+++ js/app.js
@@ -9,28 +9,42 @@ $(document).ready(function(){
   })
   .mousedown(function() {
     playHadouken();
+    $('.ryu-still').hide();
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show().animate(
-      {'left': '1020px'},
+    $('.hadouken').finish().show().animate(
+      {'left': '300px'},
       500,
       function() {
+        $(this).stop();
         $(this).hide();
-        $(this).css('left', '520px');
-    $('.hadouken').finish().show().animate(
-          {'left': '1020px'},
-          500,
-          function() {
-            $(this).hide();
-            $(this).css('left', '520ox')
-          });
-      })
+        $(this).css('left', '-212px');
+          }
+      );
     })
   .mouseup(function(){
-    $('.ryu-ready').show();
+    $('.ryu-still').show();
     $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
   });
-});
+  $(document).keydown(function(e) {
+      if (e.keyCode == 88) {
+        $('.ryu-still').hide();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').hide();
+        $('.ryu-cool').show();
+      }
+    });
+    $(document).keyup(function(e) {
+      if (e.keyCode == 88) {
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').hide();
+      }
+    })
+  });
 function playHadouken() {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
