bbec7edb0ecf23a58f889f2f68128f1832513079
Cassandra <cmcorrales3@gmail.com>
2016-06-26 00:57:24 -0500

    incorporate sound into animation

--- js/app.js
+++ js/app.js
@@ -3,8 +3,36 @@ $(document).ready(function(){
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
-  $('.ryu').mouseleave(function(){
+  .mouseleave(function(){
     $('.ryu-still').show();
     $('.ryu-ready').hide();
+  })
+  .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show().animate(
+      {'left': '1020px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '520px');
+    $('.hadouken').finish().show().animate(
+          {'left': '1020px'},
+          500,
+          function() {
+            $(this).hide();
+            $(this).css('left', '520ox')
+          });
+      })
+    })
+  .mouseup(function(){
+    $('.ryu-ready').show();
+    $('.ryu-throwing').hide();
   });
 });
+function playHadouken() {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
